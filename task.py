1507224c02c43c2683e4c5592aafbd456034e644
ancyniya2-tech <ancyniya2@gmail.com>
2026-08-25 19:05:51 +0300

    Updated project

--- task.py
+++ task.py
@@ -29,11 +29,6 @@ class BankAccount:
 
 
 account1 = BankAccount("001234",5000,"Ancy Niya","25 August 2026")
-print(type(account1))
-print(account1.account_number)
-print(account1.balance)
-print(account1.owner_name)
-print(account1.date_opened)
 account1.deposit(2000)
 account1.withdraw(1000)
 account1.display_info()
@@ -41,11 +36,6 @@ account1.display_info()
 print("--------------------------------------------------")
 
 account2 = BankAccount("005678",10000,"Jane Doe","25 August 2026")
-print(type(account2))
-print(account2.account_number)
-print(account2.balance)
-print(account2.owner_name)
-print(account2.date_opened)
 account2.deposit(2000)
 account2.withdraw(2000)
 account2.display_info()
